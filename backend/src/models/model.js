dcaf5ec8563ba2410d9adb45db01bb7c0b8f4b81
fdesai60 <fdesai60@gmail.com>
2025-06-05 10:11:38 +0100

    model.js updated

--- backend/src/models/model.js
+++ backend/src/models/model.js
@@ -1,16 +1,14 @@
 const db = require("../../db-setup/connection");
 
-const googleIdToDelete = "googl";
+// The user_id to delete
+const userIdToDelete = 2;
 
-//test db query
-db.query(`DELETE FROM usRETURNING *`, [gooDelete])
+db.query(`DELETE FROM users WHERE user_id = $1 RETURNING *`, [userIdToDelete])
   .then(result => {
     if (result.rows.length > 0) {
       console.log("Deleted user:", result.rows[0]);
     } else {
-      console.log("No user found with that google_id");
+      console.log("No user found with that user_id");
     }
   })
   .catch(err => console.error("Error deleting user:", err));
-
-
